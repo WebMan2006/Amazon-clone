21b4ac4cef11d7b7591a42372cabda585a246fec
tornike <tornikeeloshvili77@gmail.com>
2024-03-13 22:52:08 +0400

    Add: images folder and display cards on the website

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
+import { Product } from './types/product'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +14,22 @@ function App() {
         <h1>TS Amazon</h1>
       </header>
       <main>
+        <ul>
 
+        {
+          sampleProducts.map((product: Product) => (
+            <li key={product.slung}>
+              <img src={product.image} alt={product.name} className='product-image'/>
+              <h2>
+                {product.name}
+              </h2>
+              <p>
+                ${product.price}
+              </p>
+            </li>
+          ))
+        }
+        </ul>
       </main>
       <footer>
         all right reserved
