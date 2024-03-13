9af0b566dfe9175ee209345308c26c40c43528ea
tornike <tornikeeloshvili77@gmail.com>
2024-03-13 22:32:23 +0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>
+        <h1>TS Amazon</h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        all right reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
